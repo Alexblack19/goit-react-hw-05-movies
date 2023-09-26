38c38256204ac8d698753f8815bcfe392825c3dd
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-09-26 21:09:22 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { NavLink, Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home/Home';
-import { Movies } from './Movies/Movies';
+import { Movies } from '../pages/Movies/Movies';
 
 export const App = () => {
   return (
